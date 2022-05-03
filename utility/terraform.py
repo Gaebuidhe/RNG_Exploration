6ad99a1351ca0ad4f4e73769238f3c18e6921ab5
unknown <faolannus@gmail.com>
2022-05-03 17:05:04 -0400

    Update Terraform Module
    added several functions to update RegionType on tiles
    added function to update RegionTags on tiles

--- utility/terraform.py
+++ utility/terraform.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import random
+
 from utility import cartographer
 
 
@@ -7,16 +9,107 @@ def dark_water():
     return 'ocean'
 
 
-def light_water(tf_world_df, tid):
-    tile_neighbors = cartographer.find_neighbors(tf_world_df, tid)
+def light_water(tile_neighbors):
+    if " 'dark_water'" in tile_neighbors['Type'].values:
+        return 'sea'
+    else:
+        return 'lake'
+
+
+def sand():
+    return 'desert'
+
+
+def grass(tile_neighbors):
+    die_cast = random.randint(1, 100)
+    type_counts = tile_neighbors['Type'].value_counts()
+    mountain_count = 0
+    if " 'rocky'" in type_counts.keys():
+        mountain_count = type_counts[" 'rocky'"]
+
+    if die_cast >= (76 - (10 * mountain_count)):
+        return 'hills'
+    else:
+        return 'grasslands'
+
+
+def rocky():
+    return 'mountain'
+
+
+def snowy():
+    return 'arctic'
+
+
+def dark_grass(tile_neighbors):
+    die_cast = random.randint(1, 100)
+    type_counts = tile_neighbors['Type'].value_counts()
+    water_count = 0
+    if " 'water'" in type_counts.keys():
+        water_count = water_count + type_counts[" 'water'"]
+
+    if " 'dark_water'" in type_counts.keys():
+        water_count = water_count + type_counts[" 'dark_water'"]
+
+    if die_cast >= (91 - (10 * water_count)):
+        return 'swamp'
+    else:
+        return 'forest'
+
+
+def regional_tags(tile_neighbors, tid, tf_world_df):
+    region_tags = ''
+    type_counts = tile_neighbors['Type'].value_counts()
+    tid_df = tf_world_df.loc[tf_world_df['tid'] == tid]
+    if " 'water'" in type_counts.keys() or " 'dark_water'" in type_counts.keys():
+        region_tags = region_tags + 'coastal'
+
+    if tid_df['Type'].item() == " 'rocky'":
+        die_cast = random.randint(1, 100)
+        if die_cast >= 96 and region_tags == '':
+            region_tags = region_tags + 'volcano'
+        elif die_cast >= 96 and region_tags != '':
+            region_tags = region_tags + ',volcano'
+
+    underdark_die_cast = random.randint(1, 100)
+    if underdark_die_cast >= 96 and region_tags == '':
+        region_tags = region_tags + 'underdark entrance'
+    elif underdark_die_cast >= 96 and region_tags != '':
+        region_tags = region_tags + ',underdark entrance'
+
+    return region_tags
 
 
 def terraform_world(world_df):
     tf_world_df = world_df
     tf_world_df['RegionType'] = np.nan
+    tf_world_df['RegionTags'] = np.nan
     for tid in tf_world_df['tid']:
+        tile_neighbors = cartographer.find_neighbors(tf_world_df, tid)
         type_value = tf_world_df.loc[tf_world_df['tid'] == tid, 'Type'].values
         tile_type = type_value[0][2:-1]
 
         if tile_type == 'dark_water':
             tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = dark_water()
+
+        if tile_type == 'water':
+            tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = light_water(tile_neighbors)
+
+        if tile_type == 'sand':
+            tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = sand()
+
+        if tile_type == 'grass':
+            tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = grass(tile_neighbors)
+
+        if tile_type == 'dark_grass':
+            tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = dark_grass(tile_neighbors)
+
+        if tile_type == 'rocky':
+            tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = rocky()
+
+        if tile_type == 'snowy':
+            tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionType'] = snowy()
+
+        tf_world_df.loc[tf_world_df['tid'] == tid, 'RegionTags'] = regional_tags(tile_neighbors, tid, tf_world_df)
+
+    return tf_world_df
